fda748019ceeb62aef74ad3416a7da5069fe0120
Mario Basic <mario.basic@outlook.com>
2015-06-14 01:10:10 +0200

    Updated test.

--- tests/helpers/getTodaysDateInFormat.spec.js
+++ tests/helpers/getTodaysDateInFormat.spec.js
@@ -9,6 +9,6 @@ describe('getTodaysDateInFormat', function() {
     });
 
     it('should return todays date in format YYYY-MM-DD', function() {
-        expect(getTodaysDateInFormat()).to.be('2015-06-14');
+        expect(getTodaysDateInFormat()).to.equal('2015-06-14');
     });
 });
