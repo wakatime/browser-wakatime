5ff96a4f6debc99ccca8df4b54126b683d66ab32
Mario Basic <mario.basic@outlook.com>
2015-07-12 09:43:16 +0200

    A test that fails.

--- tests/helpers/changeExtensionTooltip.spec.js
+++ tests/helpers/changeExtensionTooltip.spec.js
@@ -1,4 +1,6 @@
 var chai = require('chai');
+var sinon = require('sinon');
+var chrome = require('sinon-chrome');
 var expect = chai.expect;
 
 import changeExtensionTooltip from '../../assets/js/helpers/changeExtensionTooltip';
@@ -6,5 +8,11 @@ import changeExtensionTooltip from '../../assets/js/helpers/changeExtensionToolt
 describe('changeExtensionTooltip', function() {
     it('should be a function', function() {
         expect(changeExtensionTooltip).to.be.a('function');
+    });
+
+    it('should change the extension tooltip', function() {
+        changeExtensionTooltip('WakaTime');
+
+        sinon.assert.calledWithMatch(chrome.browserAction.setTitle, {title: 'WakaTime'});
     });
 });
