07e8ae507aeeac5a4012120db01bbccec61c1074
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-07-19 01:38:58 -0400

    working on mocha tests with pahntomjs

--- tests/helpers/changeExtensionTooltip.spec.js
+++ tests/helpers/changeExtensionTooltip.spec.js
@@ -1,18 +1,38 @@
-var chai = require('chai');
-var chrome = require('sinon-chrome');
-var sinon = require('sinon-chai');
-var expect = chai.expect;
+// var chai = require('chai');
+// var chrome = require('sinon-chrome');
+// var sinon = require('sinon-chai');
+// var expect = chai.expect;
 
-import changeExtensionTooltip from '../../assets/js/helpers/changeExtensionTooltip';
+// import changeExtensionTooltip from '../../assets/js/helpers/changeExtensionTooltip';
+
+// describe('changeExtensionTooltip', function() {
+//     it('should be a function', function() {
+//         expect(changeExtensionTooltip).to.be.a('function');
+//     });
+
+//     it('should change the extension tooltip', function() {
+//         changeExtensionTooltip('WakaTime');
+//         expect(chrome.browserAction.setTitle).toHaveBeenCalledWith({title: 'Wakatime'});
+//         sinon.assert.calledWithMatch(chrome.browserAction.setTitle, {title: 'WakaTime'});
+//     });
+// });
+
+var babel 
 
 describe('changeExtensionTooltip', function() {
-    it('should be a function', function() {
-        expect(changeExtensionTooltip).to.be.a('function');
-    });
+	// sometimes it takes time to start phantomjs
+	this.timeout(4000);
 
-    it('should change the extension tooltip', function() {
-        changeExtensionTooltip('WakaTime');
-        expect(chrome.browserAction.setTitle).toHaveBeenCalledWith({title: 'Wakatime'});
-        sinon.assert.calledWithMatch(chrome.browserAction.setTitle, {title: 'WakaTime'});
-    });
+	var FILENAME = 'tests/empty.html';
+
+	it('should exist', function(done) {
+		page.open(FILENAME, function() {
+			page.injectJs('assets/js/helpers/changeExtensionTooltip.js');
+		
+			page.evaluate(function() {
+				console.log(typeof changeExtensionIcon);
+			});
+			done();
+		});
+	});
 });
