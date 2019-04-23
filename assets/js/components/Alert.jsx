a183bd3ce79705511b4f6ec0de30d222587b663a
Daniel Tschinder <daniel@tschinder.de>
2019-04-23 13:22:49 -0700

    Make create-react-class a runtime dependency and use latest version  of react-transition-group

--- assets/js/components/Alert.jsx
+++ assets/js/components/Alert.jsx
@@ -4,11 +4,6 @@ var classNames = require('classnames');
 
 var Alert = reactCreateClass({
 
-    propTypes: {
-        type: React.PropTypes.string.isRequired,
-        text: React.PropTypes.string.isRequired
-    },
-
     render: function() {
         return(
             <div className={classNames('alert', 'alert-' + this.props.type)}>{this.props.text}</div>
