65375dea78421788d7277c78caf80410a1982250
Vu Nguyen <shardphoneix@gmail.com>
2021-01-14 20:39:47 -0600

    update assets from bootstrap@3.3.7

--- xclap.ts
+++ xclap.ts
@@ -3,8 +3,7 @@ const { load, exec, concurrent, serial } = require('@xarc/run');
 load({
   build: ['postinstall', exec('gulp'), 'prettier'],
   clean: exec('rimraf public coverage vendor'),
-  // Temp run prettier twice to format some of bootstrap less files
-  prettier: [exec('prettier --write .'), exec('prettier --write .')],
+  prettier: [exec('prettier --write .')],
   lint: ['prettier'],
   postinstall: ['clean', exec('gulp postinstall')],
   test: ['build', 'lint', 'test-jest', 'test-js'],
