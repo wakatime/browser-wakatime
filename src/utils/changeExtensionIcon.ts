ce5d351522e6173dea8ad92165b37f58c21b7db0
Vu Nguyen <shardphoneix@gmail.com>
2021-01-16 19:03:30 -0600

    convert changeExtensionIcon

---
+++ src/utils/changeExtensionIcon.ts
@@ -0,0 +1,28 @@
+import { browser } from 'webextension-polyfill-ts';
+import config from '../config';
+
+type ColorIconTypes = 'gray' | 'red' | 'white';
+
+/**
+ * It changes the extension icon color.
+ */
+export default async function changeExtensionIcon(color?: ColorIconTypes): Promise<void> {
+  if (color) {
+    const path = `./graphics/wakatime-logo-38-${color}.png`;
+
+    await browser.browserAction.setIcon({
+      path: path,
+    });
+  } else {
+    const { theme } = await browser.storage.sync.get({
+      theme: config.theme,
+    });
+    const path =
+      theme === config.theme
+        ? './graphics/wakatime-logo-38.png'
+        : './graphics/wakatime-logo-38-white.png';
+    await browser.browserAction.setIcon({
+      path: path,
+    });
+  }
+}
