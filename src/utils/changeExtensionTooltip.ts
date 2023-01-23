de4baec10ea4a44dcfb9d2fcdeb060c4d5fad810
Sebastian Velez <sebasvelez@gmail.com>
2023-01-23 13:26:15 -0500

    chore: setTitle and setIcon browser actions separate by browser

--- src/utils/changeExtensionTooltip.ts
+++ src/utils/changeExtensionTooltip.ts
@@ -12,5 +12,10 @@ export default async function changeExtensionTooltip(text: string): Promise<void
     text = `${config.name} - ${text}`;
   }
 
-  await browser.action.setTitle({ title: text });
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (browser.browserAction) {
+    await browser.browserAction.setTitle({ title: text }); // Support for FF with manifest V2
+  } else {
+    await browser.action.setTitle({ title: text }); // Support for Chrome with manifest V3
+  }
 }
