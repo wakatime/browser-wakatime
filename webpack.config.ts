72b76768dd74c0caebeebedc8c3c43ddb5e2aecd
Alan Hamlett <alan.hamlett@gmail.com>
2024-02-07 09:25:32 +0100

    Use api subdomain

--- webpack.config.ts
+++ webpack.config.ts
@@ -56,7 +56,7 @@ const getConfigByBrowser = (isProd: boolean, browser: BrowserTypes): webpack.Con
         ],
       }),
       new webpack.DefinePlugin({
-        ['process.env.API_URL']: JSON.stringify('https://wakatime.com/api/v1'),
+        ['process.env.API_URL']: JSON.stringify('https://api.wakatime.com/api/v1'),
         ['process.env.CURRENT_USER_API_URL']: JSON.stringify('/users/current'),
         ['process.env.HEARTBEAT_API_URL']: JSON.stringify('/users/current/heartbeats'),
         ['process.env.LOGOUT_USER_URL']: JSON.stringify('https://wakatime.com/logout'),
